9b6ecf8df3a1e98b674bf0a426de6730a79067bd
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2018-03-07 23:41:53 +0700

    Allow hello world text to be configured by env

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
-const Koa = require('koa');
+const Koa = require("koa");
 
 const app = new Koa();
 app.use(ctx => {
-	if(ctx.request.url === '/'){
-		ctx.body = '<h1>Hello world</h1>';
-		ctx.set('Content-Type', 'text/html;charset=utf-8');
-	}else if(ctx.request.url === '/_hc/'){
-		ctx.status = 200;
-	}else{
-		ctx.body = '404';
-		ctx.status = 404;
-	}
-})
+  if (ctx.request.url === "/") {
+    ctx.body = `<h1>${process.env.TEXT || "Hello world"}</h1>`;
+    ctx.set("Content-Type", "text/html;charset=utf-8");
+  } else if (ctx.request.url === "/_hc/") {
+    ctx.status = 200;
+  } else {
+    ctx.body = "404";
+    ctx.status = 404;
+  }
+});
 
 app.listen(3000);
-console.log('http://localhost:3000');
+console.log("http://localhost:3000");
